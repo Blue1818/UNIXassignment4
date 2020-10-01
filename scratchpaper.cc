3a41281fade36297ea18a4bd9632f7acb5e8c280
Blue1818 <70177582+Blue1818@users.noreply.github.com>
2020-10-01 16:26:09 -0500

    Update scratchpaper.cc

--- scratchpaper.cc
+++ scratchpaper.cc
@@ -45,6 +45,7 @@ int main(int argc, char *argv[])
     for (int i = 1; i < argc; i++) cout << argv[i] << endl;
     cout << endl;
 
+                    //atoi(char*) returns a integer.
     cout << endl << atoi(gotparam) << endl;
 
     return 0;
