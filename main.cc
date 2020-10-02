209ed396ae18aedcfc6f439a4fa70b2cde98b27c
Blue1818 <70177582+Blue1818@users.noreply.github.com>
2020-10-02 13:45:58 -0500

    Update main.cc

--- main.cc
+++ main.cc
@@ -15,7 +15,7 @@ int main(int argc, char *argv[])
 	ssize_t nw;
 	
 	buffer[0] = 'j';
-	caesarCipher(buffer, 1, 0);
+	caesarCipher(*buffer, 1, 0);
 	return 404;
 
 	//Check if there are no arguments from the commandline.
